a353f4dc43475683fcba67e7c06934f42f22d1c5
Saxena <vishad.saxena@accenture.com>
2018-10-30 18:35:54 +0530

    commit for testing

---
+++ src/main/java/NewPracticeSessionID/newpracticesessionsID/App.java
@@ -0,0 +1,13 @@
+package NewPracticeSessionID.newpracticesessionsID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
